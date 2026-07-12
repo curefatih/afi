26d719d3695dbe976e1ad11af8848a57aa25e7e4
cüre <curefat@gmail.com>
2026-07-12 10:32:59 +0300

    feat: integrate QueryClientProvider and Toaster into root component for enhanced state management and notifications

--- web/src/routes/__root.tsx
+++ web/src/routes/__root.tsx
@@ -1,31 +1,38 @@
-import { Outlet, createRootRoute } from '@tanstack/react-router'
-import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
-import { TanStackDevtools } from '@tanstack/react-devtools'
-import { ThemeProvider } from "@/components/theme-provider"
+import { Outlet, createRootRoute } from "@tanstack/react-router";
+import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
+import { TanStackDevtools } from "@tanstack/react-devtools";
+import { ThemeProvider } from "@/components/theme-provider";
 
-import '../styles.css'
+import "../styles.css";
+import { Toaster } from "@/components/ui/sonner";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 export const Route = createRootRoute({
   component: RootComponent,
-})
+});
 
 function RootComponent() {
   return (
     <>
       <ThemeProvider defaultTheme="system" storageKey="theme">
-        <Outlet />
+        <QueryClientProvider client={queryClient}>
+          <Outlet />
+          <Toaster position="bottom-right" />
+        </QueryClientProvider>
       </ThemeProvider>
       <TanStackDevtools
         config={{
-          position: 'bottom-right',
+          position: "bottom-right",
         }}
         plugins={[
           {
-            name: 'TanStack Router',
+            name: "TanStack Router",
             render: <TanStackRouterDevtoolsPanel />,
           },
         ]}
       />
     </>
-  )
+  );
 }
