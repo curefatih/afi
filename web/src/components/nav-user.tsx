4f6f0c5ad599a04623dd2a02a50eecb7043c4474
cüre <curefat@gmail.com>
2026-07-13 22:32:37 +0300

    refactor: remove "Upgrade to Pro" option from NavUser dropdown menu

--- web/src/components/nav-user.tsx
+++ web/src/components/nav-user.tsx
@@ -134,13 +134,6 @@ export function NavUser({
             <DropdownMenuSeparator />
             <DropdownMenuGroup>
               <DropdownMenuItem>
-                <SparklesIcon />
-                Upgrade to Pro
-              </DropdownMenuItem>
-            </DropdownMenuGroup>
-            <DropdownMenuSeparator />
-            <DropdownMenuGroup>
-              <DropdownMenuItem>
                 <BadgeCheckIcon />
                 Account
               </DropdownMenuItem>
